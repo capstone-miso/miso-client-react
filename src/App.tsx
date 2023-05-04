79e60f52f76967949b221d8d6e577e59f24c0fcd
littleemm <lilysolar@naver.com>
2023-05-05 03:13:00 +0900

    [ADD]

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css';
-import { Routes, Route, BrowserRouter } from "react-router-dom"
-import RestaurantMap from './RestaurantMap';
+import "./App.css";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import RestaurantMap from "./RestaurantMap";
 
 function App() {
   return (
+    //efwef
     <BrowserRouter>
       <Routes>
-        <Route path='/test' element={<RestaurantMap />}></Route>
+        <Route path="/test" element={<RestaurantMap />}></Route>
       </Routes>
     </BrowserRouter>
   );
