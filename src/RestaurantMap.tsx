a9bab3661da41e17dc2d863e57ae87bdb37f3b84
ming-taro <hera002008@naver.com>
2023-05-05 03:02:30 +0900

    [ADD] project init

---
+++ src/RestaurantMap.tsx
@@ -0,0 +1,13 @@
+
+export default function RestaurantMap(){
+
+  return (
+    <>
+      <div className="Container">
+        <div className="Title">
+          맛집 지도
+        </div>
+      </div>
+    </>
+  )
+}
