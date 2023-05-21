9ad2b408baa4797f5beae7eb8fdb0fcd16810f52
littleemm <lilysolar@naver.com>
2023-05-21 23:44:27 +0900

    [ADD] 메인화면 추가

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,10 +1,28 @@
+import { Flex, Image, Text } from "@chakra-ui/react";
 import React from "react";
-import NaverButton from "./NaverButton";
+import DishCovery from "../../assets/DishCovery.png";
+import { Colors } from "../../constants/theme";
+import KakaoButton from "./KakaoButton";
 
 function Main() {
   return (
     <>
-      <NaverButton />
+      <Flex
+        w="390px"
+        h="844px"
+        direction="column"
+        justifyContent="center"
+        justifyItems="center"
+        alignContent="center"
+        alignItems="center"
+        background={Colors.orange[300]}
+      >
+        <Image mt="250px" w="480px" src={DishCovery} alt="로고" />
+        <KakaoButton />
+        <Text mt="50px" as="b" fontSize="md" color="white">
+          건너뛰기
+        </Text>
+      </Flex>
     </>
   );
 }
