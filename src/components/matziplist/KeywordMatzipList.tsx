5d8014cc6cf108610eb4da0b935c8f23b9326645
littleemm <lilysolar@naver.com>
2023-05-21 17:32:11 +0900

    [FIX] 키워드 리스트 ui 수정

--- src/components/matziplist/KeywordMatzipList.tsx
+++ src/components/matziplist/KeywordMatzipList.tsx
@@ -6,17 +6,14 @@ function KeywordMatzipList() {
   interface KeywordType {
     id: number;
     type: string;
+    image: string;
   }
 
   const keywordButton: KeywordType[] = [
-    { id: 0, type: "#점심에많이가는" },
-    { id: 1, type: "#저녁에많이가는" },
-    { id: 2, type: "#가성비맛집" },
-    { id: 3, type: "#회식은이곳에서" },
-    { id: 4, type: "#봄에많이찾는" },
-    { id: 5, type: "#여름에많이찾는" },
-    { id: 6, type: "#가을에많이찾는" },
-    { id: 7, type: "#겨울에많이찾는" },
+    { id: 0, type: "#가격대", image: "https://ifh.cc/g/8p8xlh.jpg" },
+    { id: 1, type: "#계절별", image: "https://ifh.cc/g/l2x5jh.jpg" },
+    { id: 2, type: "#시간대", image: "https://ifh.cc/g/l2x5jh.jpg" },
+    { id: 3, type: "#방문자", image: "https://ifh.cc/g/l2x5jh.jpg" },
   ];
 
   return (
@@ -37,7 +34,21 @@ function KeywordMatzipList() {
         >
           {keywordButton.map((KeywordType) => (
             <div key={KeywordType.id}>
-              <div className="banner">
+              <div
+                style={{
+                  width: "400px",
+                  height: "200px",
+                  backgroundImage: `url(${KeywordType.image})`,
+                  backgroundSize: "cover",
+                  position: "relative",
+                  backgroundRepeat: "no-repeat",
+                  display: "inline-block",
+                  marginTop: "10px",
+                  marginBottom: "5%",
+                  maxWidth: "100%",
+                  zIndex: "0",
+                }}
+              >
                 <div className="banner-txt">
                   <h1>{KeywordType.type}</h1>
                 </div>
